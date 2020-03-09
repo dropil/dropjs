83148dc806af1729789f057d5c8ea3235ac367bc
Jeremy McAlpine <jeremy@dropil.com>
2020-03-08 22:43:58 -0700

    Initial commit

---
+++ README.md
@@ -0,0 +1,129 @@
+# DropJS - Dropil Chain JavaScript Library
+
+An open source JavaScript library for use with Dropil Chain.
+
+The DropJS library offers the ability to create wallets, create transactions, sign transactions offline, and broadcast
+transactions to a desired RPC URL.
+
+## Installation
+
+### NPM
+
+```
+npm install @dropilcoin/dropjs
+```
+
+### Yarn
+
+```
+yarn add @dropilcoin/dropjs
+```
+
+## Import
+
+### NodeJS
+
+Import dropjs in NodeJS by placing the following line at the top of your JS file.
+``` js
+const dropjs = require("@dropilcoin/dropjs");
+```
+
+### Browser
+
+Import dropjs into a web page by placing the following JS script in the `<head>` of your document.
+``` html
+<script src='..path to drop.js'></script>
+```
+
+## Using DropJS - Basic Use
+
+Before starting, ensure you have imported the dropjs library (see above).
+
+If you are unfamiliar with any of the terminology or about how Dropil Chain functions in general, refer to the [Dropil Chain Documentation](https://docs.dropilchain.com/info)
+
+### Create a dropjs instance
+
+Create a new Drop instance by calling the `start` function of dropjs. **The `drop` variable will be referred to throughout this documentation.** Pass in a valid LCD API URL and chainId as parameters. You can pass in `http://localhost:1317` or whatever LCD URL you setup if you are running a full node on your local machine. If you omit these parameters, they will default to the values shown below. The Dropil Chain mainnet chain-id is `Dropil-Chain-Thor`.
+
+``` js
+let drop = dropjs.start(lcdUrl, chainId)
+// for example dropjs.start('https://api-dropilchain.com', 'Dropil-Chain-Thor')
+```
+
+### Create new wallet
+
+Create a new Dropil Chain wallet by calling the `generateWallet` function of dropjs.
+
+``` js
+let wallet = await dropjs.generateWallet()
+```
+
+Returns:
+``` json
+{
+  address: String,
+  mnemonic: String
+}
+```
+
+Alternatively, if you only want to generate a mnemonic and do not care about the address, you can call `generateMnemonic` which returns a mnemonic as a `String`.
+
+``` js
+let mnemonic = dropjs.generateMnemonic()
+```
+
+### Get balance of address
+
+Call the `getBalance` function to retrieve the balance of an address. By default, this function returns a `udrop` value (1000000 udrop = 1 DROP). Pass an optional second parameter of `true` to return the value represented in DROP.
+
+``` js
+// balance of drop1qs6a7ht3t2784dn9ee89nv262726v56hks3k5u is 123456789 udrop
+
+// returns '123456789' udrop
+let balanceUdrop = await drop.getBalance('drop1qs6a7ht3t2784dn9ee89nv262726v56hks3k5u')
+
+// returns '123.456789' DROP
+let balanceDrop = await drop.getBalance('drop1qs6a7ht3t2784dn9ee89nv262726v56hks3k5u', true)
+```
+
+### Send Drops
+
+DropJS makes sending a DROP transaction easy. The `send` function creates a new send transaction, signs it offline using a provided mnemonic and then broadcasts it to the LCD API URL. Sensitive information is not revealed in this process.
+
+``` js
+// the following mnemonic was generated for the purpose of this example and does not contain a balance
+let mnemonic = 'exit cross gate coconut knee border dial hat upset empower draft glide maple rain erode polar pyramid ketchup seat bone excess fault once length'
+
+// the following example will send 1000000 udrop (1 DROP) from the provided mnemonic to the address shown
+let sendResponse = await drop.send(mnemonic, 'drop1qs6a7ht3t2784dn9ee89nv262726v56hks3k5u', 1000000)
+```
+
+Optionally pass in `memo, accountNumber, sequence` after the amount parameter for advanced use. Both `accountNumber` and `sequence` must be provided when used (not just one). 
+
+The response will be the LCD API response for the broadcasted transaction.
+
+<aside class="notice">
+Note: the send, delegate, and undelegate functions use the `sync` method of signing transactions which means it waits for a response from the node, but does not wait for the transaction to be committed to the chain.
+</aside>
+
+### Delegate and Undelegate Drops
+
+Delegating and undelegating is as simple as sending Drops as shown above.
+
+``` js
+// the following mnemonic was generated for the purpose of this example and does not contain a balance
+let mnemonic = 'exit cross gate coconut knee border dial hat upset empower draft glide maple rain erode polar pyramid ketchup seat bone excess fault once length'
+
+// the following example will delegate 1000000 udrop (1 DROP) from the provided mnemonic to the validator address shown
+let delegateResponse = await drop.delegate(mnemonic, 'dropvaloper1hrd9kwhdmd730dvxpv8r44kaq2zpmtc2gzquz9', 1000000)
+
+// the following example will undelegate 1000000 udrop (1 DROP) from the validator address shown to the mnemonic provided
+// undelegated Drops are locked for 21 days before they can be withdrawn
+let undelegateResponse = await drop.undelegate(mnemonic, 'dropvaloper1hrd9kwhdmd730dvxpv8r44kaq2zpmtc2gzquz9', 1000000)
+```
+
+Optionally pass in `memo, accountNumber, sequence` after the amount parameter for advanced use. Both `accountNumber` and `sequence` must be provided when used (not just one).
+
+## Advanced Use
+
+Study `src/drop.js` to understand how this library works in order to expand into more advanced use cases. Feel free to fork this repository and create pull requests with additional useful features or for your own personal use.
---
+++ src/drop.js
@@ -0,0 +1,322 @@
+const fetch = require('node-fetch').default
+const bip39 = require('bip39')
+const bip32 = require('bip32')
+const bech32 = require('bech32')
+const secp256k1 = require('secp256k1')
+const bitcoinjs = require('bitcoinjs-lib')
+const CryptoJS = require("crypto-js")
+
+const DROP_PATH = "m/44'/118'/0'/0/0"
+const DROP_BECH32_PREFIX = 'drop'
+const TRANS_TYPE = {
+  MSG_SEND: 'cosmos-sdk/MsgSend',
+  MSG_MULTI_SEND: 'cosmos-sdk/MsgMultiSend',
+  MSG_CREATE_VALIDATOR: 'cosmos-sdk/MsgCreateValidator',
+  MSG_EDIT_VALIDATOR: 'cosmos-sdk/MsgEditValidator',
+  MSG_DELEGATE: 'cosmos-sdk/MsgDelegate',
+  MSG_UNDELEGATE: 'cosmos-sdk/MsgUndelegate',
+  MSG_BEGIN_REDELEGATE: 'cosmos-sdk/MsgBeginRedelegate',
+  MSG_WITHDRAW_DELEGATION_REWARD: 'cosmos-sdk/MsgWithdrawDelegationReward',
+  MSG_WITHDRAW_VALIDATOR_COMMISSION: 'cosmos-sdk/MsgWithdrawValidatorCommission',
+  MSG_MODIFY_WITHDRAW_ADDRESS: 'cosmos-sdk/MsgModifyWithdrawAddress',
+  MSG_SUBMIT_PROPOSAL: 'cosmos-sdk/MsgSubmitProposal',
+  MSG_DEPOSIT: 'cosmos-sdk/MsgDeposit',
+  MSG_VOTE: 'cosmos-sdk/MsgVote',
+  MSG_UNJAIL: 'cosmos-sdk/MsgUnjail'
+}
+
+/** creates a Drop instance to utilize functions on Dropil Chain */
+let Drop = function(lcdUrl, chainId) {
+  this.url = lcdUrl
+	this.chainId = chainId
+	this.path = DROP_PATH
+	this.bech32MainPrefix = DROP_BECH32_PREFIX
+}
+
+/** returns a new Drop instance to utilize functions on Dropil Chain */
+function start(lcdUrl = 'https://testnet-api.dropilchain.com', chainId = 'dropilchain-testnet') {
+  return new Drop(lcdUrl, chainId)
+}
+
+/** generates a random mnemonic for use in wallet generation */
+function generateMnemonic() {
+  let randomBytes = Buffer.from(CryptoJS.lib.WordArray.random(32).toString(), `hex`)
+  if (randomBytes.length !== 32) throw Error(`Entropy has incorrect length`)
+  
+  return bip39.entropyToMnemonic(randomBytes.toString(`hex`))
+}
+
+/** generates a new wallet and returns { address, mnemonic } */
+async function generateWallet() {
+  let mnemonic = generateMnemonic()
+  let seed = await bip39.mnemonicToSeed(mnemonic)
+  let bipInterface = bip32.fromSeed(seed)
+  let bipPath = bipInterface.derivePath(DROP_PATH)
+  let words = bech32.toWords(bipPath.identifier)
+  let address = bech32.encode(DROP_BECH32_PREFIX, words)
+  return { address, mnemonic }
+}
+
+function convertStringToBytes(str) {
+  if (typeof str !== "string") throw new Error("str expects a string")
+  	  
+	var myBuffer = []
+	var buffer = Buffer.from(str, 'utf8')
+	for (var i = 0; i < buffer.length; i++) {
+	    myBuffer.push(buffer[i])
+  }
+  
+	return myBuffer
+}
+
+function getPubKeyBase64(privateKey) {
+	const pubKeyByte = secp256k1.publicKeyCreate(privateKey)
+	return Buffer.from(pubKeyByte, 'binary').toString('base64')
+}
+
+function sortObject(obj) {
+	if (obj === null) return null
+	if (typeof obj !== "object") return obj
+  if (Array.isArray(obj)) return obj.map(sortObject)
+  
+	const sortedKeys = Object.keys(obj).sort()
+	const result = {}
+	sortedKeys.forEach(key => {
+		result[key] = sortObject(obj[key])
+  })
+  
+	return result
+}
+
+/** returns response from LCD API (this.url) on /auth/accounts/{address} endpoint */
+Drop.prototype.getAccount = function(address) {
+  return fetch(this.url + "/auth/accounts/" + address)
+  .then(response => response.json())
+}
+
+/** returns { accountNumber, sequence } for provided mnemonic */
+Drop.prototype.getAccountData = async function(mnemonic) {
+  let address = await this.getAddress(mnemonic)
+  return this.getAccount(address)
+  .then(data => ({ accountNumber: String(data.result.value.account_number), sequence: String(data.result.value.sequence) }))
+}
+
+/** returns String(accountNumber) from provided mnemonic */
+Drop.prototype.getAccountNumber = async function(mnemonic) {
+  let address = await this.getAddress(mnemonic)
+  return this.getAccount(address)
+  .then(data => String(data.result.value.account_number))
+}
+
+/** returns String(sequence) from provided mnemonic */
+Drop.prototype.getAccountSequence = async function(mnemonic) {
+  let address = await this.getAddress(mnemonic)  
+  return this.getAccount(address)
+  .then(data => String(data.result.value.sequence))
+}
+
+/** returns { address, privateKey } based on provided mnemonic */
+Drop.prototype.getKeys = async function(mnemonic) {
+  return {
+    address: await this.getAddress(mnemonic),
+    privateKey: await this.getPrivateKey(mnemonic)
+  }
+}
+
+/** returns address for provided mnemonic */
+Drop.prototype.getAddress = async function(mnemonic) {
+  if (typeof mnemonic !== "string") throw new Error("mnemonic expects a string")
+  
+	const seed = await bip39.mnemonicToSeed(mnemonic)
+	const node = bip32.fromSeed(seed)
+	const child = node.derivePath(this.path)
+  const words = bech32.toWords(child.identifier)
+  const address = bech32.encode(this.bech32MainPrefix, words)
+
+	return address
+}
+
+/** returns privateKey for provided mnemonic */
+Drop.prototype.getPrivateKey = async function(mnemonic) {
+  if (typeof mnemonic !== "string") throw new Error("mnemonic expects a string")
+  
+	const seed = await bip39.mnemonicToSeed(mnemonic)
+	const node = bip32.fromSeed(seed)
+	const child = node.derivePath(this.path)
+  const ecpair = bitcoinjs.ECPair.fromPrivateKey(child.privateKey, { compressed : false })
+  
+	return ecpair.privateKey
+}
+
+/** returns udrop balance of provided address; set convert to true to return in DROP decimal format */
+Drop.prototype.getBalance = async function(address, convert = false) {
+  let data = await this.getAccount(address)
+
+  // return 0 if error or udrop does not exist
+  if (!data.result || !data.result.value.coins.length || 
+    !data.result.value.coins.filter(c => c.denom === 'udrop').length) return '0'
+
+  let balance = data.result.value.coins.filter(c => c.denom === 'udrop')[0].amount
+
+  return convert ? String(parseFloat(balance) / 1000000.0) : balance
+}
+
+/** returns a stdMsg based on the input json */
+function newStdMsg(input) {
+	return {
+    json: input,
+    bytes: convertStringToBytes(JSON.stringify(sortObject(input)))
+  }
+}
+
+Drop.prototype.buildStdMsg = function(type, params, data, memo = '') {
+  return newStdMsg({
+    msgs: [
+      {
+        type,
+        value: { ...params }
+      }
+    ],
+    chain_id: this.chainId,
+    fee: { amount: [ { amount: String(1000000), denom: "udrop" } ], gas: String(200000) },
+    memo,
+    account_number: String(data.accountNumber),
+    sequence: String(data.sequence)
+  })
+}
+
+/** signs a transaction offline using provided privateKey */
+function sign(stdMsg, privateKey, modeType = "sync") {
+  // Mode types: 
+  // "block" (return after tx commit)
+  // "sync" (return afer CheckTx)
+  // "async" (return right away)
+
+	let signMessage = stdMsg.json
+	
+	const hash = crypto.createHash('sha256').update(JSON.stringify(sortObject(signMessage))).digest('hex')
+	const buf = Buffer.from(hash, 'hex')
+	let signObj = secp256k1.ecdsaSign(buf, privateKey)
+  let signatureBase64 = Buffer.from(signObj.signature, 'binary').toString('base64')  
+  
+  return {
+    "tx": {
+        "msg": stdMsg.json.msgs,
+        "fee": stdMsg.json.fee,
+        "signatures": [
+            {
+                "signature": signatureBase64,
+                "pub_key": {
+                    "type": "tendermint/PubKeySecp256k1",
+                    "value": getPubKeyBase64(privateKey)
+                }
+            }
+        ],
+        "memo": stdMsg.json.memo
+    },
+    "mode": modeType
+  }
+}
+
+/** broadcasts signed transaction to LCD API (this.url) */
+Drop.prototype.broadcast = async function(signedTx) {
+	const response = await fetch(this.url + "/txs", {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json'
+    },
+    body: JSON.stringify(signedTx)
+  })
+
+  return await response.json()
+}
+
+/** 
+ * creates a send transaction & signs offline using provided mnemonic and then 
+ * broadcasts to LCD API (this.url);
+ * optionally pass in accountNumber and sequence for manual override 
+ */
+Drop.prototype.send = async function(mnemonic, toAddress, amount, memo = '', accountNumber = null, sequence = null) {
+  let { address, privateKey } = await this.getKeys(mnemonic)  
+
+  let data = { accountNumber, sequence }
+  if (!accountNumber || !sequence) data = await this.getAccountData(mnemonic)
+  
+  let params = {
+    amount: [
+      {
+        amount: String(amount),
+        denom: "udrop"
+      }
+    ],
+    from_address: address,
+    to_address: toAddress
+  }
+
+  let stdMsg = this.buildStdMsg(TRANS_TYPE.MSG_SEND, params, data, memo)
+  const signedTx = sign(stdMsg, privateKey)
+  
+  return await this.broadcast(signedTx)
+}
+
+/** 
+ * creates a delegate transaction & signs offline using provided mnemonic and then 
+ * broadcasts to LCD API (this.url);
+ * optionally pass in accountNumber and sequence for manual override 
+ */
+Drop.prototype.delegate = async function(mnemonic, validatorAddress, amount, memo = '', accountNumber = null, sequence = null) {
+  let { address, privateKey } = await this.getKeys(menmonic)
+
+  let data = { accountNumber, sequence }
+  if (!accountNumber || !sequence) data = await this.getAccountData(mnemonic)
+  
+  let params = {
+    amount: {
+      amount: String(amount),
+      denom: "udrop"
+    },
+    delegator_address: address,
+    validator_address: validatorAddress
+  }
+
+  let stdMsg = this.buildStdMsg(TRANS_TYPE.MSG_DELEGATE, params, data, memo)
+  const signedTx = sign(stdMsg, privateKey)
+
+  return await this.broadcast(signedTx)
+}
+
+/** 
+ * creates an undelegate transaction & signs offline using provided mnemonic and then 
+ * broadcasts to LCD API (this.url);
+ * optionally pass in accountNumber and sequence for manual override 
+ */
+Drop.prototype.undelegate = async function(mnemonic, validatorAddress, amount, memo = '', accountNumber = null, sequence = null) {
+  let { address, privateKey } = await this.getKeys(menmonic)
+
+  let data = { accountNumber, sequence }
+  if (!accountNumber || !sequence) data = await this.getAccountData(mnemonic)
+  
+  let params = {
+    amount: {
+      amount: String(amount),
+      denom: "udrop"
+    },
+    delegator_address: address,
+    validator_address: validatorAddress
+  }
+
+  let stdMsg = this.buildStdMsg(TRANS_TYPE.MSG_UNDELEGATE, params, data, memo)
+  const signedTx = sign(stdMsg, privateKey)
+
+  return await this.broadcast(signedTx)
+}
+
+/** 
+ * only export the start & generateWallet functions because all other
+ * functions should be invoked using the variable created when calling dropjs.create()
+ */
+module.exports = {
+  start,
+  generateMnemonic,
+  generateWallet
+}
